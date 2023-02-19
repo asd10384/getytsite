76ea58fc57f08c55baa7e95ae1c1072ebf1f2914
tkrmagid <tmdgks0466@naver.com>
2023-02-19 20:06:52 +0900

    오류수정

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,16 +1,14 @@
 import "dotenv/config";
 
 export const key = "AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30";
-export const contentClientVersion = "1.20221019.01.00";
+export const contentClientVersion = process.env.YOUTUBE_CONTENTCLIENTVERSION;
 
-export const A = {
-  visitorData: process.env.A_YOUTUBE_MUSIC_VISITORDATA,
+export const Adata = {
   authorization: process.env.A_YOUTUBE_MUSIC_AUTHORIZATION,
   cookie: process.env.A_YOUTUBE_MUSIC_COOKIE
 }
 
-export const B = {
-  visitorData: process.env.B_YOUTUBE_MUSIC_VISITORDATA,
+export const Bdata = {
   authorization: process.env.B_YOUTUBE_MUSIC_AUTHORIZATION,
   cookie: process.env.B_YOUTUBE_MUSIC_COOKIE
 }
